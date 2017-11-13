db372a8c205927a303a8489e14d3253dfcc0da72
Marc Sànchez <msanxes@gmail.com>
2017-11-13 14:32:54 +0100

    [WIP] PReparat per que en feng treballi

--- MIp1-mi.c
+++ MIp1-mi.c
@@ -1,9 +1,415 @@
-/**
-aquest fitxer ha de contenir el codi que implementa el protocol PMI, amb les crides a les funcions
-de la interfície de sockets TCP (a través d’incloure llibreries com sys/types.h, sys/socket.h, etc.). No
-haurà de tenir res de codi “relatiu” a la interfície aplicació-usuari via teclat i pantalla (excepte potser
-en casos d’error). Tindrà funcions internes i externes. Les funcions externes, en termes d’una
-arquitectura en capes, seran el que coneixem com la interfície de la capa MI
-*/
+/**************************************************************************/
+/*                                                                        */
+/* P1 - MI amb sockets TCP/IP - Part I                                    */
+/* Fitxer mi.c que implementa la capa d'aplicació de MI, sobre la capa de */
+/* transport TCP (fent crides a la interfície de la capa TCP -sockets-).  */
+/* Autors: Marc Sánchez , Feng Lin                                                           */
+/*                                                                        */
+/**************************************************************************/
+
+/* Inclusió de llibreries, p.e. #include <sys/types.h> o #include "meu.h" */
+/*  (si les funcions externes es cridessin entre elles, faria falta fer   */
+/*   un #include "MIp1v4-mi.h")                                           */
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "MIp1-mi.h"
+
+/* Definició de constants, p.e., #define MAX_LINIA 150                    */
+#define ALL_INTERFACES 			"0.0.0.0"
+
+/* Declaració de funcions internes que es fan servir en aquest fitxer     */
+/* (les seves definicions es troben més avall) per així fer-les conegudes */
+/* des d'aqui fins al final de fitxer.                                    */
+int TCP_CreaSockClient(const char *IPloc, int portTCPloc);
+int TCP_CreaSockServidor(const char *IPloc, int portTCPloc);
+int TCP_DemanaConnexio(int Sck, const char *IPrem, int portTCPrem);
+int TCP_AcceptaConnexio(int Sck, char *IPrem, int *portTCPrem);
+int TCP_Envia(int Sck, const char *SeqBytes, int LongSeqBytes);
+int TCP_Rep(int Sck, char *SeqBytes, int LongSeqBytes);
+int TCP_TancaSock(int Sck);
+int TCP_TrobaAdrSockLoc(int Sck, char *IPloc, int *portTCPloc);
+int TCP_TrobaAdrSockRem(int Sck, char *IPrem, int *portTCPrem);
+int HaArribatAlgunaCosa(const int *LlistaSck, int LongLlistaSck);
+void MostraError(const char *text);
 
 
+/* Definicio de funcions EXTERNES, és a dir, d'aquelles que en altres     */
+/* fitxers externs es faran servir.                                       */
+/* En termes de capes de l'aplicació, aquest conjunt de funcions externes */
+/* formen la interfície de la capa MI.                                    */
+
+/* Inicia l’escolta de peticions remotes de conversa a través d’un nou    */
+/* socket TCP en el #port “portTCPloc” i una @IP local qualsevol (és a    */
+/* dir, crea un socket “servidor” o en estat d’escolta – listen –).       */
+/* Retorna -1 si hi ha error; l’identificador del socket d’escolta de MI  */
+/* creat si tot va bé.                                                    */
+int MI_IniciaEscPetiRemConv(int portTCPloc)
+{
+    TCP_CreaSockServidor(ALL_INTERFACES, portTCPloc);
+}
+
+/* Escolta indefinidament fins que arriba una petició local de conversa   */
+/* a través del teclat o bé una petició remota de conversa a través del   */
+/* socket d’escolta de MI d’identificador “SckEscMI” (un socket           */
+/* “servidor”).                                                           */
+/* Retorna -1 si hi ha error; 0 si arriba una petició local; SckEscMI si  */
+/* arriba una petició remota.                                             */
+int MI_HaArribatPetiConv(int SckEscMI)
+{
+    
+}
+
+/* Crea una conversa iniciada per una petició local que arriba a través   */
+/* del teclat: crea un socket TCP “client” (en un #port i @IP local       */
+/* qualsevol), a través del qual fa una petició de conversa a un procés   */
+/* remot, el qual les escolta a través del socket TCP ("servidor") d'@IP  */
+/* “IPrem” i #port “portTCPrem” (és a dir, crea un socket “connectat” o   */
+/* en estat establert – established –). Aquest socket serà el que es farà */
+/* servir durant la conversa.                                             */
+/* Omple “IPloc*” i “portTCPloc*” amb, respectivament, l’@IP i el #port   */
+/* TCP del socket del procés local.                                       */
+/* El nickname local “NicLoc” i el nickname remot són intercanviats amb   */
+/* el procés remot, i s’omple “NickRem*” amb el nickname remot. El procés */
+/* local és qui inicia aquest intercanvi (és a dir, primer s’envia el     */
+/* nickname local i després es rep el nickname remot).                    */
+/* "IPrem" i "IPloc*" són "strings" de C (vectors de chars imprimibles    */
+/* acabats en '\0') d'una longitud màxima de 16 chars (incloent '\0').    */
+/* "NicLoc" i "NicRem*" són "strings" de C (vectors de chars imprimibles  */
+/* acabats en '\0') d'una longitud màxima de 300 chars (incloent '\0').   */
+/* Retorna -1 si hi ha error; l’identificador del socket de conversa de   */
+/* MI creat si tot va bé.                                                 */
+int MI_DemanaConv(const char *IPrem, int portTCPrem, char *IPloc, int *portTCPloc, const char *NicLoc, char *NicRem)
+{
+
+}
+
+/* Crea una conversa iniciada per una petició remota que arriba a través  */
+/* del socket d’escolta de MI d’identificador “SckEscMI” (un socket       */
+/* “servidor”): accepta la petició i crea un socket (un socket            */
+/* “connectat” o en estat establert – established –), que serà el que es  */
+/* farà servir durant la conversa.                                        */
+/* Omple “IPrem*”, “portTCPrem*”, “IPloc*” i “portTCPloc*” amb,           */
+/* respectivament, l’@IP i el #port TCP del socket del procés remot i del */
+/* socket del procés local.                                               */
+/* El nickname local “NicLoc” i el nickname remot són intercanviats amb   */
+/* el procés remot, i s’omple “NickRem*” amb el nickname remot. El procés */
+/* remot és qui inicia aquest intercanvi (és a dir, primer es rep el      */
+/* nickname remot i després s’envia el nickname local).                   */
+/* "IPrem*" i "IPloc*" són "strings" de C (vectors de chars imprimibles   */
+/* acabats en '\0') d'una longitud màxima de 16 chars (incloent '\0').    */
+/* "NicLoc" i "NicRem*" són "strings" de C (vectors de chars imprimibles  */
+/* acabats en '\0') d'una longitud màxima de 300 chars (incloent '\0').   */
+/* Retorna -1 si hi ha error; l’identificador del socket de conversa      */
+/* de MI creat si tot va bé.                                              */
+int MI_AcceptaConv(int SckEscMI, char *IPrem, int *portTCPrem, char *IPloc, int *portTCPloc, const char *NicLoc, char *NicRem)
+{
+
+}
+
+/* Escolta indefinidament fins que arriba una línia local de conversa a   */
+/* través del teclat o bé una línia remota de conversa a través del       */
+/* socket de conversa de MI d’identificador “SckConvMI” (un socket        */
+/* "connectat”).                                                          */
+/* Retorna -1 si hi ha error; 0 si arriba una línia local; SckConvMI si   */
+/* arriba una línia remota.                                               */
+int MI_HaArribatLinia(int SckConvMI)
+{
+
+}
+
+/* Envia a través del socket de conversa de MI d’identificador            */
+/* “SckConvMI” (un socket “connectat”) la línia “Linia” escrita per       */
+/* l’usuari local.                                                        */
+/* "Linia" és un "string" de C (vector de chars imprimibles acabat en     */
+/* '\0'), no conté el caràcter fi de línia ('\n') i té una longitud       */
+/* màxima de 300 chars (incloent '\0').                                   */
+/* Retorna -1 si hi ha error; el nombre de caràcters n de la línia        */
+/* enviada (sense el ‘\0’) si tot va bé (0 <= n <= 299).                  */
+int MI_EnviaLinia(int SckConvMI, const char *Linia)
+{
+
+}
+
+/* Rep a través del socket de conversa de MI d’identificador “SckConvMI”  */
+/* (un socket “connectat”) una línia escrita per l’usuari remot, amb la   */
+/* qual omple “Linia”, o bé detecta l’acabament de la conversa per part   */
+/* de l’usuari remot.                                                     */
+/* "Linia*" és un "string" de C (vector de chars imprimibles acabat en    */
+/* '\0'), no conté el caràcter fi de línia ('\n') i té una longitud       */
+/* màxima de 300 chars (incloent '\0').                                   */
+/* Retorna -1 si hi ha error; -2 si l’usuari remot acaba la conversa; el  */
+/* nombre de caràcters n de la línia rebuda (sense el ‘\0’) si tot va bé  */
+/* (0 <= n <= 299).                                                       */
+int MI_RepLinia(int SckConvMI, char *Linia)
+{
+
+}
+
+/* Acaba la conversa associada al socket de conversa de MI                */
+/* d’identificador “SckConvMI” (un socket “connectat”).                   */
+/* Retorna -1 si hi ha error; un valor positiu qualsevol si tot va bé.    */
+int MI_AcabaConv(int SckConvMI)
+{
+
+}
+
+/* Acaba l’escolta de peticions remotes de conversa que arriben a través  */
+/* del socket d’escolta de MI d’identificador “SckEscMI” (un socket       */
+/* “servidor”).                                                           */
+/* Retorna -1 si hi ha error; un valor positiu qualsevol si tot va bé.    */
+int MI_AcabaEscPetiRemConv(int SckEscMI)
+{
+
+}
+
+
+/* Definicio de funcions INTERNES, és a dir, d'aquelles que es faran      */
+/* servir només en aquest mateix fitxer.                                  */
+
+/* Crea un socket TCP “client” a l’@IP “IPloc” i #port TCP “portTCPloc”   */
+/* (si “IPloc” és “0.0.0.0” i/o “portTCPloc” és 0 es fa/farà una          */
+/* assignació implícita de l’@IP i/o del #port TCP, respectivament).      */
+/* "IPloc" és un "string" de C (vector de chars imprimibles acabat en     */
+/* '\0') d'una longitud màxima de 16 chars (incloent '\0').               */
+/* Retorna -1 si hi ha error; l’identificador del socket creat si tot     */
+/* va bé.                                                                 */
+int TCP_CreaSockClient(const char *IPloc, int portTCPloc)
+{
+    int fd;
+	if((fd=socket(AF_INET,SOCK_STREAM,0))==-1)
+	{
+		close(fd);
+
+		return (-1);
+	}
+
+	return fd;
+}
+
+/* Crea un socket TCP “servidor” (o en estat d’escolta – listen –) a      */
+/* l’@IP “IPloc” i #port TCP “portTCPloc” (si “IPloc” és “0.0.0.0” i/o    */
+/* “portTCPloc” és 0 es fa una assignació implícita de l’@IP i/o del      */
+/* #port TCP, respectivament).                                            */
+/* "IPloc" és un "string" de C (vector de chars imprimibles acabat en     */
+/* '\0') d'una longitud màxima de 16 chars (incloent '\0').               */
+/* Retorna -1 si hi ha error; l’identificador del socket creat si tot     */
+/* va bé.                                                                 */
+int TCP_CreaSockServidor(const char *IPloc, int portTCPloc)
+{
+    //printf("Anem a configurar %s al port %i\n", IPloc, portTCPloc);
+
+	int fd;
+	if((fd=socket(AF_INET,SOCK_STREAM,0))==-1)
+	{
+		close(fd);
+		return -1;
+	}
+
+	int i;
+	struct sockaddr_in adrloc;
+	adrloc.sin_family=AF_INET;
+	adrloc.sin_port=htons(portTCPloc);
+	adrloc.sin_addr.s_addr=inet_addr(IPloc); /* o bé: ...s_addr = INADDR_ANY */
+	for(i=0;i<8;i++){adrloc.sin_zero[i]='0';}
+
+	if((bind(fd,(struct sockaddr*)&adrloc,sizeof(adrloc)))==-1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	if((listen(fd,3))==-1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	return fd;
+}
+
+/* El socket TCP “client” d’identificador “Sck” demana una connexió al    */
+/* socket TCP “servidor” d’@IP “IPrem” i #port TCP “portTCPrem” (si tot   */
+/* va bé es diu que el socket “Sck” passa a l’estat “connectat” o         */
+/* establert – established –).                                            */
+/* "IPrem" és un "string" de C (vector de chars imprimibles acabat en     */
+/* '\0') d'una longitud màxima de 16 chars (incloent '\0').               */
+/* Retorna -1 si hi ha error; un valor positiu qualsevol si tot va bé.    */
+int TCP_DemanaConnexio(int Sck, const char *IPrem, int portTCPrem)
+{
+    struct sockaddr_in adrConnexio;
+	adrConnexio.sin_family=AF_INET;
+	adrConnexio.sin_port=htons(portTCPrem);
+	adrConnexio.sin_addr.s_addr=inet_addr(IPrem); /* o bé: ...s_addr = INADDR_ANY */
+	int i;
+	for(i=0;i<8;i++){adrConnexio.sin_zero[i]='0';}
+
+	if((connect(Sck,(struct sockaddr*)&adrConnexio,sizeof(adrConnexio)))==-1)
+	{
+		close(Sck);
+		return (-1);
+	}
+	return Sck; // El socket serà un valor positiu.
+}
+
+/* El socket TCP “servidor” d’identificador “Sck” accepta fer una         */
+/* connexió amb un socket TCP “client” remot, i crea un “nou” socket,     */
+/* que és el que es farà servir per enviar i rebre dades a través de la   */
+/* connexió (es diu que aquest nou socket es troba en l’estat “connectat” */
+/* o establert – established –; el nou socket té la mateixa adreça que    */
+/* “Sck”).                                                                */
+/* Omple “IPrem*” i “portTCPrem*” amb respectivament, l’@IP i el #port    */
+/* TCP del socket remot amb qui s’ha establert la connexió.               */
+/* "IPrem*" és un "string" de C (vector de chars imprimibles acabat en    */
+/* '\0') d'una longitud màxima de 16 chars (incloent '\0').               */
+/* Retorna -1 si hi ha error; l’identificador del socket connectat creat  */
+/* si tot va bé.                                                          */
+int TCP_AcceptaConnexio(int Sck, char *IPrem, int *portTCPrem)
+{
+    int socket;
+
+	struct sockaddr_in adrConnexio;
+	int long_adrrem=sizeof(adrConnexio);
+
+	if((socket=accept(Sck,(struct sockaddr*)&adrConnexio, &long_adrrem))==-1)
+	{
+		close(Sck);
+		return (-1);
+	}
+
+	// S'ha establert la connexió i s'emplenen les dades de qui s'ha connectat a les variables d'entrada.
+	IPrem = inet_ntoa(adrConnexio.sin_addr);
+	portTCPrem = (int *) (int) ntohs(adrConnexio.sin_port);
+	return socket;
+}
+
+/* Envia a través del socket TCP “connectat” d’identificador “Sck” la     */
+/* seqüència de bytes escrita a “SeqBytes” (de longitud “LongSeqBytes”    */
+/* bytes) cap al socket TCP remot amb qui està connectat.                 */
+/* "SeqBytes" és un vector de chars qualsevol (recordeu que en C, un      */
+/* char és un enter de 8 bits) d'una longitud >= LongSeqBytes bytes.      */
+/* Retorna -1 si hi ha error; el nombre de bytes enviats si tot va bé.    */
+int TCP_Envia(int Sck, const char *SeqBytes, int LongSeqBytes)
+{
+    return write(Sck,SeqBytes,strlen(SeqBytes));
+}
+
+/* Rep a través del socket TCP “connectat” d’identificador “Sck” una      */
+/* seqüència de bytes que prové del socket remot amb qui està connectat,  */
+/* i l’escriu a “SeqBytes*” (que té una longitud de “LongSeqBytes” bytes),*/
+/* o bé detecta que la connexió amb el socket remot ha estat tancada.     */
+/* "SeqBytes*" és un vector de chars qualsevol (recordeu que en C, un     */
+/* char és un enter de 8 bits) d'una longitud <= LongSeqBytes bytes.      */
+/* Retorna -1 si hi ha error; 0 si la connexió està tancada; el nombre de */
+/* bytes rebuts si tot va bé.                                             */
+int TCP_Rep(int Sck, char *SeqBytes, int LongSeqBytes)
+{
+    return read(Sck, SeqBytes, LongSeqBytes);
+}
+
+/* S’allibera (s’esborra) el socket TCP d’identificador “Sck”; si “Sck”   */
+/* està connectat es tanca la connexió TCP que té establerta.             */
+/* Retorna -1 si hi ha error; un valor positiu qualsevol si tot va bé.    */
+int TCP_TancaSock(int Sck)
+{
+    return close(Sck);
+}
+
+/* Donat el socket TCP d’identificador “Sck”, troba l’adreça d’aquest     */
+/* socket, omplint “IPloc*” i “portTCPloc*” amb respectivament, la seva   */
+/* @IP i #port TCP.                                                       */
+/* "IPloc*" és un "string" de C (vector de chars imprimibles acabat en    */
+/* '\0') d'una longitud màxima de 16 chars (incloent '\0').               */
+/* Retorna -1 si hi ha error; un valor positiu qualsevol si tot va bé.    */
+int TCP_TrobaAdrSockLoc(int Sck, char *IPloc, int *portTCPloc)
+{
+    struct sockaddr_in adrrem;
+	int long_adrrem=sizeof(adrrem);
+	if (getsockname(Sck, (struct sockaddr *)&adrrem, &long_adrrem) == -1)
+	{
+		close(Sck);
+		return -1;
+	}
+	strcpy(IPloc, inet_ntoa(adrrem.sin_addr));
+	//portTCPloc = (int*)ntohs(adrrem.sin_port);
+	int res[1];
+	res[0] = ntohs(adrrem.sin_port);
+	portTCPloc = res;
+}
+
+/* Donat el socket TCP “connectat” d’identificador “Sck”, troba l’adreça  */
+/* del socket remot amb qui està connectat, omplint “IPrem*” i            */
+/* “portTCPrem*” amb respectivament, la seva @IP i #port TCP.             */
+/* "IPrem*" és un "string" de C (vector de chars imprimibles acabat en    */
+/* '\0') d'una longitud màxima de 16 chars (incloent '\0').               */
+/* Retorna -1 si hi ha error; un valor positiu qualsevol si tot va bé.    */
+int TCP_TrobaAdrSockRem(int Sck, char *IPrem, int *portTCPrem)
+{
+    struct sockaddr_in adrrem;
+	int long_adrrem=sizeof(adrrem);
+	if (getpeername(Sck, (struct sockaddr *)&adrrem, &long_adrrem) == -1)
+	{
+		close(Sck);
+		return -1;
+	}
+	strcpy(IPrem, inet_ntoa(adrrem.sin_addr));
+	int res[1];
+	res[0] = ntohs(adrrem.sin_port);
+	portTCPrem = res;
+}
+
+/* Examina simultàniament i sense límit de temps (una espera indefinida)  */
+/* els sockets (poden ser TCP, UDP i stdin) amb identificadors en la      */
+/* llista “LlistaSck” (de longitud “LongLlistaSck” sockets) per saber si  */
+/* hi ha arribat alguna cosa per ser llegida.                             */
+/* "LlistaSck" és un vector d'enters d'una longitud >= LongLlistaSck      */
+/* Retorna -1 si hi ha error; si arriba alguna cosa per algun dels        */
+/* sockets, retorna l’identificador d’aquest socket.                      */
+int HaArribatAlgunaCosa(const int *LlistaSck, int LongLlistaSck)
+{
+    fd_set conjunt;
+	FD_ZERO(&conjunt);
+	int i;
+	int descmax = 0;
+	for(i = 0; i < LongLlistaSck; i++){
+		int fd = LlistaSck[i];
+		FD_SET(fd,&conjunt);
+		if(fd > descmax){
+			descmax = fd;
+		}
+	}
+
+	/* examinem lectura del teclat i del socket scon amb la llista conjunt */
+	if(select(descmax+1, &conjunt, NULL, NULL, NULL) == -1)
+	{
+		perror("Error en select");
+		return (-1);
+	}
+	// Ha d'haver arrivat algu per algun dels sockets
+	int j;
+	for(j = 0; j < LongLlistaSck; j++){
+		if(FD_ISSET(LlistaSck[j], &conjunt)){
+			//printf("FD seleccionat : %i \n", LlistaSck[j]);
+			return LlistaSck[j];
+		}
+	}
+	// Si surt del bucle vol dir que hi ha hagut un error ja que cap dels sockets ha saltat i no pot ser.
+	return -1;
+}
+
+/* Escriu un missatge de text al flux d’error estàndard stderr, format    */
+/* pel text “Text”, un “:”, un espai en blanc, un text que descriu el     */
+/* darrer error produït en una crida de sockets, i un canvi de línia.     */
+void MostraError(const char *text)
+{
+    fprintf(stderr, "%s: %s\n", text, strerror(errno));
+}
+
+
+/* Si ho creieu convenient, feu altres funcions...                        */
--- MIp1-p2p.c
+++ MIp1-p2p.c
@@ -1,6 +1,104 @@
-/** 
-- La interfície aplicació-usuari (vegeu la Fig. 1) ha d’estar implementada en el fitxer “MIp1-p2p.c”, és
-a dir, aquest fitxer ha de contenir el programa principal (main), amb el codi relatiu a la interfície
-aplicació-usuari (via teclat i pantalla), i les crides a la interfície de la capa MI (a través d’incloure
-“MIp1-mi.h”). No haurà de tenir res de codi “relatiu” als sockets, ni relatiu al protocol PMI. 
+/**************************************************************************/
+/*                                                                        */
+/* P1 - MI amb sockets TCP/IP - Part I                                    */
+/* Fitxer p2p.c que implementa la interfície aplicació-usuari de          */
+/* l'aplicació de MI, sobre la capa d'aplicació de MI (fent crides a la   */
+/* interfície de la capa MI -fitxers mi.c i mi.h-).                       */
+/* Autors: Marc Sánchez, Feng Lin                                                         */
+/*                                                                        */
+/**************************************************************************/
+
+
+/* Inclusió de llibreries, p.e. #include <stdio.h> o #include "meu.h"     */
+/* Incloem "MIp1v4-mi.h" per poder fer crides a la interfície de MI       */
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "MIp1-mi.c"
+
+/* Definició de constants, p.e., #define MAX_LINIA 150                      */
+#define TECLAT 				0
+
+/* Definició de màxims de longitud dels diferents tipus d'string            */
+#define MAX_BUFFER 			200
+#define MAX_TYPE 			2
+#define MAX_IP 				16
+#define MAX_LINE			204
+#define MAX_PORT			5
+
+/* Definició de funcions per el programa principal                          */
+void EvalResult(int res, const int *sockets, int nSockets);
+int getPort();
+
+
+int main(int argc,char *argv[])
+{
+    /* Declaració de variables, p.e., int n;                                */
+    int port;
+    int socketEscoltar;
+
+    // INICI
+    port = getPort();
+
+    
+ }
+
+/***************** FUNCIONS D'EVALUACIÓ ************************************/
+
+/**
+* Evalua res, si és negatiu tenca els sockets i acaba l'apliacació, altrament no fa res.
+* - res : és el resultat que s'ha d'evaluar
+* - sockets : son tots els sockets que l'aplicació té oberts
+* - nSockets : és el nombre de sockets que l'aplicació té oberts
+* En cas que hagi tencat l'aplicació haurà mostrat per pantalla l'error que s'ha produït.
 */
+void EvalResult(int res, const int *sockets, int nSockets){
+	if(res < 0){
+		int i = 0;
+		// Faltaria buidar el buffer del TECLAT que sempre el poso a la posició 0 de sockets.
+		while(sockets != NULL && i<nSockets){
+				close(sockets[i]);
+				i++;
+		}
+		MostraError("Error ");
+		exit (-1);
+	}
+}
+
+/***************** FUNCIONS DE LECTURA ************************************/
+
+/**
+ * Llegeix de teclat numberBytesToRead caràcters i els posa a inData
+ * Retorna un -1 si quelcom ha anat malament.
+ * Retorna el nombre de bytes llegits si tot ha anat bé, inData conté la cadena de caràcters llegida.
+ */
+int readFromKeyboard(char * inData, int numberBytesToRead){
+	int bytes_llegits;
+	if((bytes_llegits=read(0, inData, numberBytesToRead))==-1)
+	{
+    return (-1);
+	}
+	printf("%i\n", bytes_llegits);
+	inData[bytes_llegits-1] = '\0';
+	printf("%s\n",inData);
+
+    return bytes_llegits;
+}
+
+/**
+ * Llegeix un número de teclat que fa referència al port.
+ * Retorna aquest número com un enter positiu > 0.
+ * Si hi ha error retorna -1.
+ */
+int getPort(){
+	char port[MAX_BUFFER];
+	bzero(port, '\0');
+	printf("%s\n", "Entra el número de port que ha de fer servir el programa \nHa de ser un enter positiu entre 0 i 65000 : ");
+	int res = readFromKeyboard(port, MAX_PORT);
+	printf("Has entrat el port %s\n",port);
+	EvalResult(res, NULL, 0); // Indiquem que no hi ha sockets amb un 0 i així no petarà.
+	int portNumber;
+	EvalResult(portNumber = atoi(port), NULL, 0); // indiquem que no hi ha sockets amb un 0 i així no petarà.
+	return portNumber;
+}
